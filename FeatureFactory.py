ba185aa85c284e06870ee46f3b41772207085ad1
jmwebb <jmwebb@stanford.edu>
2015-12-05 22:50:42 -0800

    added FeatureFactory

--- FeatureFactory.py
+++ FeatureFactory.py
@@ -5,3 +5,20 @@ Author: James Webb (jmwebb@stanford.edu)
 Created: 12/5/2015
 """
 
+featureList = list()
+userFeatures = dict()
+
+def __init__(self):
+	featureList.append('BetweenessCentrality');
+	featureList.append('DegreeCentrality');
+	featureList.append('ClosenessCentrality');
+
+def addUser(self, userId):
+	userFeatures[userId] = [0]*len(featureList)
+
+def addFeature(self, userId, featureName, value):
+	if featureName in featureList:
+		userFeatures[userId][featureIndexfeatureList().index(featureName)] = value
+
+def getFeatureMatrix(self):
+	return userFeatures
