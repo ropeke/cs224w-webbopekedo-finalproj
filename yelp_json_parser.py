5c1047637f568906017ba1eade9b52002c786e0b
Kelvin <kelvindo@stanford.edu>
2015-12-07 21:25:14 -0800

    Generated plots for AZ and PZ distributions

--- yelp_json_parser.py
+++ yelp_json_parser.py
@@ -10,9 +10,9 @@ Modified: 11/11/2015
 
 import sys
 import json
-import networkx as nx
 from re import sub
 import cPickle as pickle
+import matplotlib.pyplot as plt
 
 columnSeparator = "|"
 raw_friendship_map = {} # unsanitized friendship map including dead nodes
@@ -23,10 +23,7 @@ business_map = {}
 pair_map = {}
 business_reviews = {}
 
-#The graph
-YGraph = nx.Graph()
-
-MIN_EDGES = 5
+MIN_EDGES = 0
 
 """
 Parses a single json file. Currently, there's a loop that iterates over each
@@ -51,75 +48,27 @@ def parseJson(json_file):
 def initializeGraph():
     print "initializeGraph()"
     for user in raw_friendship_map:
-        YGraph.add_node(user, type="user")
         if user not in friendship_map:
             friendship_map[user] = []
         friends = raw_friendship_map[user]
         for friend in friends:
             if friend in raw_friendship_map:
-                YGraph.add_edge(user, friend)
                 friendship_map[user].append(friend)
 
-    print "Calculating Centarlities"
-    degree_centrality_map = {} #nx.degree_centrality(YGraph)
-    print len(degree_centrality_map)
-    closeness_centrality_map = {} #nx.closeness_centrality(YGraph)
-    print len(closeness_centrality_map)
-    betweenness_centrality_map = {} #nx.betweenness_centrality(YGraph)
-    print len(betweenness_centrality_map)
+    degree_list = [len(friendship_map[user]) for user in friendship_map]
 
-    print "The number of nodes in the graph is: %d" % YGraph.number_of_nodes()
-    print "The number of edges in the graph is: %d" % YGraph.number_of_edges()
-    print "Density: %f" % (YGraph.number_of_edges() * 1.0 / YGraph.number_of_nodes())
+    degree_counts = [0 for _ in range(0, 1530)]
 
-    # used for testing purposes to figure out range of community minimum sizes that I should have!
-    # print "Finding Communities"
-    # for test_variable in range(2,11):
-    #     communities = list(nx.k_clique_communities(YGraph, test_variable))
-    #     print "Number of communities: %d" % len(communities)
-    #     #total_size = 0
-        #for c in communities:
-        #    print len(c)
-        #    total_size += len(c)
-        #print "Total size: %d" % total_size
-    return (degree_centrality_map, closeness_centrality_map, betweenness_centrality_map)
+    for degree in degree_list:
+        degree_counts[degree] += 1
 
-
-# """
-# calculateSimilarities - Calculated the similarities between pairs of nodes. We take all pairs of friends of friends to calculate similarities for
-# """
-# def calculateSimilarities():
-#     print "calculateSimilarities()"
-#     # loop through friends
-#     for user in friendship_map:
-#         friends = friendship_map[user]
-#         for friend in friends:
-#             pair_map[(user, friend)] = similarityFriendshipOverlap(user, friend)
-#             if friend in friendship_map:
-#                 # loop through friends of friends
-#                 friend_friends = friendship_map[friend]
-#                 for friend_friend in friend_friends:
-#                     if (user, friend_friend) not in pair_map:
-#                         pair_map[(user, friend_friend)] = similarityFriendshipOverlap(user, friend_friend)
-
-#     print "Number of pairs: %d" % len(pair_map)
-
-
-# """
-# similarityFriendshipOverlap - Finds the similaritiy of two nodes by comparing the
-# fraction of their friends that overlap
-# """
-# def similarityFriendshipOverlap(node1, node2):
-#     if node1 not in friendship_map or node2 not in friendship_map:
-#         return 0
-
-#     friends1 = frozenset(friendship_map[node1])
-#     friends2 = frozenset(friendship_map[node2])
-
-#     overlap = friends1.intersection(friends2)
-#     union = friends1.union(friends2)
-
-#     return len(overlap) * 1.0 / len(union)
+    print degree_counts
+    plt.loglog(degree_counts, '.')
+    plt.xlabel('Degree')
+    plt.ylabel('Number of Nodes')
+    plt.title('PA Degree Distribution')
+    plt.show()
+    print max(degree_list)
 
 
 def processReviews():
@@ -134,31 +83,6 @@ def processReviews():
 
     print "Number of businesses reviewed: %d" % len(business_reviews)
 
-def loadFromFile():
-    """
-    To save time, loads desired information from file
-    """
-    try:
-        f1 = open( "friendshipMap.p", "rb" )
-        friendship_map = pickle.load(f1)
-        f1.close()
-        f2 = open( "businessReviews.p", "rb" )
-        business_reviews = pickle.load(f2)
-        f2.close()
-        f3 = open( "degreeCentrality.p", "rb" )
-        degree_centrality_map = pickle.load(f3)
-        f3.close()
-        f4 = open( "closenessCentrality.p", "rb" )
-        closeness_centrality_map = pickle.load(f4)
-        f4.close()
-        f5 = open( "betweennessCentrality.p", "rb" )
-        betweenness_centrality_map = pickle.load(f5)
-        f5.close()
-    except IOError as e:
-        sys.stderr.write("I/O error({0}): {1}".format(e.errno, e.strerror)+'\n')
-        sys.stderr.write('Try running with -buildClean = clean!\n')
-
-    return (friendship_map, business_reviews)
 
 
 # UPDATE by James: now called by RunYelpPredictor, returns values, separate from similarity
@@ -187,16 +111,5 @@ def parseJsons(businessJson='pa_business.json', reviewJson='pa_review.json', use
 
     # we won't need the graph for the first part
     # UPDATE by James: initialized because needed to populate friendship_map
-    (degree_centrality_map, closeness_centrality_map, betweenness_centrality_map) = initializeGraph()
-
-    # business reviews are in the global business_reviews
-    processReviews()
-
-    pickle.dump(friendship_map, open( "friendshipMap.p", "wb" ) )
-    pickle.dump(business_reviews, open( "businessReviews.p", "wb" ) )
-    pickle.dump(degree_centrality_map, open( "degreeCentrality.p", "wb" ) )
-    pickle.dump(closeness_centrality_map, open( "closenessCentrality.p", "wb" ) )
-    pickle.dump(betweenness_centrality_map, open( "betweennessCentrality.p", "wb" ) )
-
-    return (friendship_map, business_reviews, degree_centrality_map, closeness_centrality_map, betweenness_centrality_map, YGraph)
+    initializeGraph()
 
